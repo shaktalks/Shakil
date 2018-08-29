1548affd19e120b5fa1e87cab6e6756489acd4d3
shaktalks <42296904+shaktalks@users.noreply.github.com>
2018-08-28 20:12:50 -0400

    Create forLoop.js

---
+++ forLoop.js
@@ -0,0 +1,7 @@
+let name= []
+for( let i = 13; i < 35; i++){
+ name += $('.header')[i].innerText + '\n';
+}
+console.log(name);
+
+//make i whatever number it is in the array. the goal is to get a list of all the names by looping through the elements
